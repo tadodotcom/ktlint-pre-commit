f125092b4ff1fa339db5194510d405d14bdc3117
Timo Streckfuss <timo.streckfuss@tado.com>
2020-03-23 22:04:08 +0100

    run command on root level

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -2,17 +2,16 @@
 
 REPOSITORY="https://github.com/pinterest/ktlint"
 VERSION=0.41.0
-KTLINT="ktlint-${VERSION}"
+CACHE=".cache"
+KTLINT="${CACHE}/ktlint-${VERSION}"
 
-mkdir -p .cache
-cd .cache
+mkdir -p "${CACHE}"
 if [ ! -f "${KTLINT}" ]
 then
     curl -SLf "${REPOSITORY}/releases/download/$VERSION/ktlint" > "${KTLINT}"
     chmod 755 "${KTLINT}"
 fi
-cd ..
 
 changed_kotlin_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*kt$" )
 echo $changed_kotlin_files
-.cache/${KTLINT} changed_kotlin_files
+${KTLINT} changed_kotlin_files
