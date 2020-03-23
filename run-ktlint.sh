bbd3313effb8712842a5c2e8713b35dcefa705fe
Timo Streckfuss <timo.streckfuss@tado.com>
2020-03-23 21:59:00 +0100

    use versioned executable artefacts

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
+
+REPOSITORY="https://github.com/pinterest/ktlint"
+VERSION=0.41.0
+KTLINT="ktlint-${VERSION}"
+
 mkdir -p .cache
 cd .cache
-if [ ! -f ktlint ]
+if [ ! -f "${KTLINT}" ]
 then
-    curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.41.0/ktlint
-    chmod 755 ktlint
+    curl -SLf "${REPOSITORY}/releases/download/$VERSION/ktlint" > "${KTLINT}"
+    chmod 755 "${KTLINT}"
 fi
 cd ..
 
 changed_kotlin_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*kt$" )
 echo $changed_kotlin_files
-.cache/ktlint $changed_kotlin_files
+.cache/${KTLINT} changed_kotlin_files
