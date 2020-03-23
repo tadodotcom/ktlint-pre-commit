9536c03d008de7fc63e62fd7ae21f929ded14d2b
Timo Streckfuss <timo.streckfuss@tado.com>
2020-03-23 21:37:04 +0100

    update ktlint to latest release
    the ktlint repository has been moved over to the pinterest account.

--- run-ktlint.sh
+++ run-ktlint.sh
@@ -3,7 +3,7 @@ mkdir -p .cache
 cd .cache
 if [ ! -f ktlint ]
 then
-    curl -sSLO https://github.com/shyiko/ktlint/releases/download/0.29.0/ktlint
+    curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.41.0/ktlint
     chmod 755 ktlint
 fi
 cd ..
